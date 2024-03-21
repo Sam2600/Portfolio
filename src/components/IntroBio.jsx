e8dbe6646ffe07060baa1a0db4e217ec342eee31
Kaung Htet San <kaunghtet.san@brycenmyanmar.com.mm>
2024-03-21 14:35:24 +0630

    added framer motion in bio section fade in

--- src/components/IntroBio.jsx
+++ src/components/IntroBio.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 import samprofile from "../assets/samprofile.jpg";
 import { FaLinkedin, FaFacebook, FaGithub } from "react-icons/fa";
 
 export const IntroBio = () => {
   return (
-    <section className="py-5">
+    <motion.section
+      className="py-5"
+      initial={{ opacity: 0, y: "-10vw" }}
+      animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+      exit={{ opacity: 0 }}
+    >
+      {/* <section className="py-5"> */}
       <div className="container max-w-screen-xl mx-auto px-4">
         <div className="text-center">
           <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
@@ -46,6 +53,7 @@ export const IntroBio = () => {
           </div>
         </div>
       </div>
-    </section>
+      {/* </section> */}
+    </motion.section>
   );
 };
