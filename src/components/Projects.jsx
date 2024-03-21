53c685e4f90e1d64e0993be08d6fdf49ed1d5dca
Kaung Htet San <kaunghtet.san@brycenmyanmar.com.mm>
2024-03-21 14:37:31 +0630

    fix project NaN minor bug

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,86 +4,79 @@ import { FaReact, FaLaravel, FaDatabase, FaDocker } from "react-icons/fa";
 
 export const Projects = () => {
   return (
-    (
-      <section className="py-5">
-        <h1
-          id="my-project"
-          class="inline-block mx-5 pb-3 mb-10 text-4xl font-bold text-gray-900 capitalize border-b-4 border-slate-500"
-        >
-          My projects
-        </h1>
-        <div class="container max-w-screen-xl mx-auto px-4">
-          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div class="bg-gray-50 p-8 rounded-md shadow-sm border cursor-pointer hover:shadow-lg">
+    <section className="py-5">
+      <h1
+        id="my-project"
+        class="inline-block mx-5 pb-3 mb-10 text-4xl font-bold text-gray-900 capitalize border-b-4 border-slate-500"
+      >
+        My projects
+      </h1>
+      <div class="container max-w-screen-xl mx-auto px-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div class="bg-gray-50 p-8 rounded-md shadow-sm border cursor-pointer hover:shadow-lg">
+            <a
+              href="https://movie-nerd-sam2600.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="border" src={movieNerds} alt="" />
+            </a>
+
+            <h4 class="mt-3 text-lg mb-4 text-black font-bold">Movie Nerds</h4>
+
+            <p class="font-normal text-gray-500 text-md">
+              A website with variety of movies categories and specific details
+              with decent descriptions, rating and recommendations.
+              <br />
+              <br />
+              Tech stacks are ReactJs, Redux Toolkit, Tailwind CSS and other
+              libraries.
+            </p>
+            <div className="flex items-center justify-between">
               <a
                 href="https://movie-nerd-sam2600.vercel.app/"
                 target="_blank"
-                rel="noreferrer"
+                class="inline-flex items-center gap-1 mt-4 font-medium group text-sky-500"
               >
-                <img className="border" src={movieNerds} alt="" />
+                Live Demo
+                <span
+                  aria-hidden="true"
+                  class="block transition-all group-hover:ms-0.5 rtl:rotate-180"
+                >
+                  →
+                </span>
               </a>
-
-              <h4 class="mt-3 text-lg mb-4 text-black font-bold">
-                Movie Nerds
-              </h4>
-
-              <p class="font-normal text-gray-500 text-md">
-                A website with variety of movies categories and specific details
-                with decent descriptions, rating and recommendations.
-                <br />
-                <br />
-                Tech stacks are ReactJs, Redux Toolkit, Tailwind CSS and other
-                libraries.
-              </p>
-              <div className="flex items-center justify-between">
-                <a
-                  href="https://movie-nerd-sam2600.vercel.app/"
-                  target="_blank"
-                  class="inline-flex items-center gap-1 mt-4 font-medium group text-sky-500"
+              <a
+                href="https://github.com/Sam2600/Movie-Nerd-with-React"
+                target="_blank"
+                class="inline-flex items-center gap-1 mt-4 font-medium group text-sky-500"
+              >
+                Source code
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 496 512"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  Live Demo
-                  <span
-                    aria-hidden="true"
-                    class="block transition-all group-hover:ms-0.5 rtl:rotate-180"
-                  >
-                    →
-                  </span>
-                </a>
-                <a
-                  href="https://github.com/Sam2600/Movie-Nerd-with-React"
-                  target="_blank"
-                  class="inline-flex items-center gap-1 mt-4 font-medium group text-sky-500"
-                >
-                  Source code
-                  <svg
-                    stroke="currentColor"
-                    fill="currentColor"
-                    stroke-width="0"
-                    viewBox="0 0 496 512"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
-                  </svg>
-                </a>
-              </div>
+                  <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
+                </svg>
+              </a>
             </div>
           </div>
         </div>
+      </div>
 
-        <div class=" mt-20 flex items-center text-xs text-black uppercase before:flex-[1_1_0%] before:border-t before:border-black before:me-6 after:flex-[1_1_0%] after:border-t after:border-black after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">
-          <div className="flex justify-center gap-10">
-            <FaReact className="text-3xl transition-all duration-200 hover:text-blue-600 cursor-pointer" />
-            <FaLaravel className="text-3xl transition duration-200 hover:text-blue-600 cursor-pointer" />
-            <FaDocker className="text-3xl transition duration-200 hover:text-blue-600 cursor-pointer" />
-            <FaDatabase className="text-3xl transition duration-200 hover:text-blue-600 cursor-pointer" />
-          </div>
+      <div class=" mt-20 flex items-center text-xs text-black uppercase before:flex-[1_1_0%] before:border-t before:border-black before:me-6 after:flex-[1_1_0%] after:border-t after:border-black after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">
+        <div className="flex justify-center gap-10">
+          <FaReact className="text-3xl transition-all duration-200 hover:text-blue-600 cursor-pointer" />
+          <FaLaravel className="text-3xl transition duration-200 hover:text-blue-600 cursor-pointer" />
+          <FaDocker className="text-3xl transition duration-200 hover:text-blue-600 cursor-pointer" />
+          <FaDatabase className="text-3xl transition duration-200 hover:text-blue-600 cursor-pointer" />
         </div>
-      </section>
-    ) *
-    {
-      /* </motion.section> */
-    }
+      </div>
+    </section>
   );
 };
