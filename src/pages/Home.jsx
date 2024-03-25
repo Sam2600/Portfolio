364d2b55341e4ce2b8b7e387b0f451708f69dbde
Sam2600 <kghtetsan26@gmail.com>
2024-03-25 22:50:44 +0630

    added scrolled to top in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IntroBio } from "../components/IntroBio";
 import { Projects } from "../components/Projects";
 import { WorkExperience } from "../components/WorkExperience";
 
 export const Home = () => {
+  //
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
   return (
     <>
       <IntroBio />
